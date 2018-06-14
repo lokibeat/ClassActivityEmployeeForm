24dedc7cdd594ffbd557605b2aa3925c43a468cf
Tom Sampson <tbsampson@gmail.com>
2018-06-13 20:42:26 -0500

    added some table function

--- employeeformcode.js
+++ employeeformcode.js
@@ -1,3 +1,5 @@
+var database = firebase.database();
+
 $("#add-user").on("click", function(event) {
     // Don't refresh the page!
     event.preventDefault();
@@ -11,9 +13,46 @@ $("#add-user").on("click", function(event) {
     console.log(rate);
 
     database.ref().push({
-    employeeName: employeeName,
-    employeeRole: employeeRole,
-    startDate: startDate,
-    rate: rate
+        employeeName: employeeName,
+        employeeRole: employeeRole,
+        startDate: startDate,
+        rate: rate
     });
+
+   displayEmployee(employeeName, employeeRole, startDate, rate);
+
 });
+
+
+
+function displayEmployee(name, role, start, rate) {
+    let worked = 0;
+    let billed = 0;
+
+    $('#full-employee-list').html(`
+    <table class="table">
+    <tr>
+        <th>Employee Name</th>
+        <th>Role</th>
+        <th>Start Date</th>
+        <th>Months Worked</th>
+        <th>Monthly Rate ($)</th>
+        <th>Total BIlled ($)</th>
+    </tr>
+    `)
+
+    $('#full-employee-list').append(`
+    <tr>
+        <td>${name}</td>
+        <td>${role}</td>
+        <td>${start}</td>
+        <td>${worked}</td>
+        <td>${rate}</td>
+        <td>${billed}</td>
+    </tr>
+    `)
+
+    $('#full-employee-list').append(`
+    </table>`)    
+}
+
